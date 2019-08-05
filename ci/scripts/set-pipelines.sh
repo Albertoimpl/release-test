746d66385e74ae4eb28db4fadd2649394d78a544
Alberto Rios <arios@pivotal.io>
2019-08-05 13:42:37 +0200

    Adding pipeline definition and docker image

---
+++ ci/scripts/set-pipelines.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+set_pipeline() {
+  echo "Setting release pipeline..."
+  fly -t scs set-pipeline -p release-test -c pipeline.yml -l config.yml
+}
+
+main() {
+  fly -t scs sync
+
+  pushd "$(dirname "$0")/.." >/dev/null
+
+  set_pipeline
+
+  popd >/dev/null
+}
+
+main
